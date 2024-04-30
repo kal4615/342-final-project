0982266797ec476600887fa8c91e83bf0cd29b2c
Carter Vail <cjv1558@rit.edu>
2024-04-30 15:40:06 -0400

    Enter and Leave stubs

--- PitStop.java
+++ PitStop.java
@@ -1,11 +1,16 @@
+import java.util.LinkedList;
+
 public class PitStop {
     private int MAX_CARS = 3;
     private int location;
     private int current_cars;
+    private LinkedList<Car> pit_queue = new LinkedList<Car>();
 
     public PitStop(int loc){
         this.location = loc;
         this.current_cars = 0;
     }
+
+
 
 }
