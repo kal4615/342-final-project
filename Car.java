942d3968721595fd0992a21acb8dc337f1c2d147
Kevin Luong <kal4615@rit.edu>
2024-04-30 15:50:33 -0400

    changed some var types

--- Car.java
+++ Car.java
@@ -1,12 +1,12 @@
-public class Car {
+public class Car extends Thread{
     private int ID;
     private int max_speed;
     private int current_speed;
-    private float speed_decay;
+    private double speed_decay;
     private int pit_time;
     private int position;
 
-    public Car(int ID, int speed, float decay, int pit){
+    public Car (int ID, int speed, double decay, int pit){
         this.ID = ID;
         this.max_speed = speed;
         this.current_speed = speed;
@@ -14,11 +14,21 @@ public class Car {
         this.pit_time = pit;
         this.position = 0;
     }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        // tell race manager to move car distance of current speed
+        // decay speed current_speed = (int)(current_speed * speed_decay);
+    }
     
-    public static void Drive (){
+    // default drive function that will decay the speed of the car as it drives
+    public void Drive (){
+        current_speed = (int)(current_speed * speed_decay);
 
     }
 
+    // formula to decide if car uses next stop or not
     public static void Next_Pit_Stop(){
 
     }
